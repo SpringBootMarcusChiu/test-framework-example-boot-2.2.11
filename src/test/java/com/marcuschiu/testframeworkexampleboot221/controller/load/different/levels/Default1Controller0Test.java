adeb009bd905119c06de2776a55bb74d90758604
Marcus Chiu <marcuschiu9@gmail.com>
2020-12-02 18:55:25 -0600

    initial commit

---
+++ src/test/java/com/marcuschiu/testframeworkexampleboot221/controller/load/different/levels/Default1Controller0Test.java
@@ -0,0 +1,23 @@
+package com.marcuschiu.testframeworkexampleboot221.controller.load.different.levels;
+
+import com.marcuschiu.testframeworkexampleboot221.controller.Default1Controller;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Test Whether Controller is loaded into app context
+ */
+@SpringBootTest
+public class Default1Controller0Test {
+
+    @Autowired
+    Default1Controller controller;
+
+    @Test
+    public void contextLoads() {
+        assertNotNull(controller);
+    }
+}
---
+++ src/test/java/com/marcuschiu/testframeworkexampleboot221/controller/load/different/levels/Default1Controller4Test.java
@@ -0,0 +1,33 @@
+package com.marcuschiu.testframeworkexampleboot221.controller.load.different.levels;
+
+import com.marcuschiu.testframeworkexampleboot221.controller.Default1Controller;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @WebMvcTest(Default1Controller.class) - Test Controller by only loading a specific Controller in the web layer
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest(Default1Controller.class)
+public class Default1Controller4Test {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @org.junit.Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, World")));
+    }
+}
