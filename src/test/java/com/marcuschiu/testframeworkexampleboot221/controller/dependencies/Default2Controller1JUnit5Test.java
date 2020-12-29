31ee0563f301f2fb15032f425c75d729dce293f3
Marcus Chiu <marcuschiu9@gmail.com>
2020-12-28 23:02:52 -0600

    added controller test example

--- src/test/java/com/marcuschiu/testframeworkexampleboot221/controller/dependencies/Default2Controller1JUnit5Test.java
+++ src/test/java/com/marcuschiu/testframeworkexampleboot221/controller/dependencies/Default2Controller1JUnit5Test.java
@@ -3,9 +3,11 @@ package com.marcuschiu.testframeworkexampleboot221.controller.dependencies;
 import com.marcuschiu.testframeworkexampleboot221.controller.Default2Controller;
 import com.marcuschiu.testframeworkexampleboot221.service.Default1Service;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
@@ -23,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  *   - @AutoConfigureMockMvc - adds MockMvc into application context
  *   - @ImportAutoConfiguration -
  */
+//@ExtendWith({SpringExtension.class}) included in @WebMvcTest
 @WebMvcTest(Default2Controller.class)
 public class Default2Controller1JUnit5Test {
 
